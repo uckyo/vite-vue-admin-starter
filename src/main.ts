2685f81df7a8a0a6e0e2b70a0d08347b23c60e73
蒋文亮 <10244284+uckyo@user.noreply.gitee.com>
2024-03-23 07:05:45 +0800

    配置自动导入icon

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import {createApp} from 'vue'
-import router from './router'
-import './style.css'
-import 'element-plus/dist/index.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router";
+import "./style.css";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
 
+app.use(router);
+app.mount("#app");
